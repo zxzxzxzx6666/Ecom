6e90d5af28e7e059551c39e0ca026ff58c92607c
Kailunlin <zzzooogtytw@gmail.com>
2025-05-17 20:15:28 +0800

    主頁第一次上板無圖片

---
+++ Web/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.eShopWeb.Web;
+
+/// <summary>
+/// 設定參數
+/// </summary>
+public static class Constants
+{
+    public const string BASKET_COOKIENAME = "eShop";
+    public const int ITEMS_PER_PAGE = 10;
+    public const string DEFAULT_USERNAME = "Guest";
+    public const string BASKET_ID = "BasketId";
+}
--- Web/ViewModels/Home/IndexViewModel.cs
+++ Web/ViewModels/Home/IndexViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace Web.ViewModels.Home
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.ViewModels.Home
 {
     public class IndexViewModel
     {
         public List<CatalogItemViewModel> CatalogItems { get; set; } = new List<CatalogItemViewModel>();
+        public List<SelectListItem>? Brands { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem>? Types { get; set; } = new List<SelectListItem>();
+        public int? BrandFilterApplied { get; set; }
+        public int? TypesFilterApplied { get; set; }
+        public PaginationInfoViewModel? PaginationInfo { get; set; }
     }
 }
