6d6f734f96c9d4769641845e1924b78a79820b86
Ming <Ming@DESKTOP-IG0E2NN>
2025-08-23 09:56:49 +0800

    [AddItemtoBasket] connect web success

---
+++ Web/wwwroot/js/Home/index.js
@@ -0,0 +1,29 @@
+﻿function AddItemToBasket(button) {
+    // 找出與這個按鈕在同一個 form 內的四個 hidden 欄位的值
+    var $form = $(button).closest('form');
+    var data = {
+        id: $form.find('input[name="id"]').val(),
+        name: $form.find('input[name="name"]').val(),
+        pictureUri: $form.find('input[name="pictureUri"]').val(),
+        price: $form.find('input[name="price"]').val()
+    };
+
+    $.ajax({
+        url: '/Basket/AddItemToBasket',
+        type: 'POST',
+        data: data,
+        success: function (response) {
+            if (response.success) {
+                alert(response.message);
+            } else {
+                alert('操作失敗');
+            }
+        },
+        error: function () {
+            alert('發生錯誤');
+        }
+    });
+
+    // 阻止表單送出
+    return false;
+}
